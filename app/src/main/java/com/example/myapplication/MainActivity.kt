5ce834236e710c833f7778331031756e66b2b3c0
taruc <delsonsds-jm17@student.tarc.edu.my>
2020-03-05 12:35:25 +0800

    second

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,25 +3,73 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImage : ImageView
+    //lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val diceImage : ImageView = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val dice_image: ImageView = findViewById(R.id.dice_image)
     }
 
     private fun rollDice(){
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        //val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+       val drawableRes = when (randomInt){
+           1 -> R.drawable.dice_1
+           2 -> R.drawable.dice_2
+           3 -> R.drawable.dice_3
+           4 -> R.drawable.dice_4
+           5 -> R.drawable.dice_5
+           else -> R.drawable.dice_6
+       }
+        diceImage.setImageResource(drawableRes)
+
+       /* resultText.text = randomInt.toString()
+
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
+
+
+        if(resultText.text == "Hello World!"){
+            resultText.text = "1"
+        }
+        else{
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6){
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }*/
+
+        diceImage.setImageResource(drawableRes)
+
+        diceImage.setImageResource(imagerand())
+
+
+    }
+    private fun imagerand(): Int{
+        val randomInt = (1..6).random()
+        val drawableRes = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return drawableRes
 
     }
 }
